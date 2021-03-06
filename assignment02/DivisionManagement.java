afdb397c031ce2744133037a10120e2908140bfb
harrisonwang2 <harrisonwang2@gmail.com>
2021-03-05 20:21:58 -0500

    'divisionManagement'

--- assignment02/DivisionManagement.java
+++ assignment02/DivisionManagement.java
@@ -1,32 +1,42 @@
 package assignment02;
 
 public abstract class DivisionManagement{
-	private Map<String, Collection<Employee>> listing = new TreeMap<>();
+	// map division name to division leads
+	private Map<String, Collection<Employee>> divisionLeads = new TreeMap<>();
+
+	// map divsion name to division employees
+	private Map<String, Collection<Employee>> divisionEmployees = new TreeMap<>();
 	
-	// only for UpperManagement :
-	private Collection<Person> boardMembers = new TreeSet<>();
-	public void addBoardMember(Person person) {
-		boardMembers.add(person);
+	// only for Division Management:
+
+	// add new divison
+	public void addDivision(String title) {
+		if(!divisionLeads.containsKey(title))
+			divisionLeads.put(title, new TreeSet<>());
+			divisionEmployees.put(title, new TreeSet<>());
 	}
 	
-	// concrete subclasses actually create the titles
-	public void addJobTitle(String title) {
-		if(!listing.containsKey(title))
-			listing.put(title, new TreeSet<>());
+	// add new lead to division
+	public void addLeads(String title, Employee emp) {
+		if(!divisionLeads.containsKey(title))
+			throw new IllegalArgumentException("This division name does not exist");
+		divisionLeads.get(title).add(emp);:
 	}
 	
-	public void addEmployeeToRole(String jobTitle, Employee emp) {
-		if(!listing.containsKey(jobTitle))
-			throw new IllegalArgumentException("This Job Title does not exist");
-		listing.get(jobTitle).add(emp);
+	// add new employee to division
+	public void addEmployeeToDivision(String title, Employee emp) {
+		if(!divisionleads.containsKey(Title))
+			throw new IllegalArgumentException("This division name does not exist");
+		divisionEmployees.get(title).add(emp);
 	}
 
-	public void addEmployeeToRoleUsingId(String jobTitle, int empId) {
-		if(!listing.containsKey(jobTitle))
-			throw new IllegalArgumentException("This Job Title does not exist");
+	// add new employee to division using ID
+	public void addEmployeeToDivisionUsingId(String title, int empId) {
+		if(!divisionLeads.containsKey(title))
+			throw new IllegalArgumentException("This division name does not exist");
 		EmployeeDataBase db = EmployeeDataBase.getEmployeeDataBase(); 
 		if(db.isEmployeeId(empId))
-			listing.get(jobTitle).add(db.getFromId(empId));
+			divisionEmployees.get(title).add(db.getFromId(empId));
 	}
 	
 }
