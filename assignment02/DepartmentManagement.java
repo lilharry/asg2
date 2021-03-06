ad0556cd19a5385dbbbf8408cc439fb92f6b3647
Harry Lum <hlum1@binghamton.edu>
2021-03-05 20:06:06 -0500

    working on department management

--- assignment02/DepartmentManagement.java
+++ assignment02/DepartmentManagement.java
@@ -1,16 +1,29 @@
 package assignment02;
 
 public abstract class DepartmentManagement {
-	private Map<String, Collection<Employee>> listing = new TreeMap<>();
+
+	//map department name to employee head
+	private Map<String, Collection<Employee>> departmentHeads = new TreeMap<>();
+
+	//map department name to employees in department
+	private Map<String, Collection<Employee>> departmentEmployees = new TreeMap<>();
 	
-	// only for UpperManagement :
-	private Collection<Person> boardMembers = new TreeSet<>();
-	public void addBoardMember(Person person) {
-		boardMembers.add(person);
+	//map department name to divisions in department
+	private Map<String, Collection<String>> departmentDivisions = new TreeMap<>();
+
+	// only for DepartmentManagement :
+
+	public void addDepartment(String title) {
+		if(!departmentHeads.containsKey(title)){
+			//add department
+			departmentHeads.put(title, new TreeSet<>());
+			departmentEmployees.put(title, new TreeSet<>());
+			departmentDivisions.put(title, new TreeSet<>());
+		}
 	}
 	
-	// concrete subclasses actually create the titles
-	public void addJobTitle(String title) {
+	// add head to department
+	public void addHead(String title, Employee head) {
 		if(!listing.containsKey(title))
 			listing.put(title, new TreeSet<>());
 	}
